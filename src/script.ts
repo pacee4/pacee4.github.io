b3ffd38b21e709640794c47fe210625029dbb430
pacee4 <165809246+pacee4@users.noreply.github.com>
2026-06-23 10:17:16 +0300

    Update info

--- src/script.ts
+++ src/script.ts
@@ -8,7 +8,7 @@ import * as B from "@/base_script.js";
     const birthDate = B.getPeriodUntilToday("22.05.2006");
     const years = birthDate.years;
     const periodText = `${years} ${B.sklonenieNoun(years, "год", "года", "лет")}`;
-    if (birthDate.days === 0) {
+    if (birthDate.days === 0 && birthDate.months === 0) {
         // День рождения
         vcDatePeriod.textContent = `(${periodText}!)`;
         vcDatePeriod.classList.add("js-animatedDatePeriod");
